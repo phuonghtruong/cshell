fddabdf607e0a4470e73bbe96b96b7571a144b80
Brian Truong <phuongtruong.h@gmail.com>
2019-05-28 16:53:41 -0700

    run c/cpp and python filename

--- script_sh/run_shell.sh
+++ script_sh/run_shell.sh
@@ -5,13 +5,15 @@
 # Author                : Brian Truong
 # Version   Date        Description
 # 1.0       May 26      Run bash script with permission execution
+# 2.0       May 28      Run C/C++ filename
+#                       Run Python filename
 #
 #
 # Plans:
 #       1. Run bash script
 #       2. Run Python
 #       3. Run C/C++
-###############################################################################
+#################################################################
 
 # Check if shell script is executable and run it
 run_shell(){
@@ -21,14 +23,41 @@ run_shell(){
         chmod +x $1
         ./$1 "${@:2}"
     fi
+
+    echo
+    echo "Finished !!!"
 }
 
 run_py(){
-    echo "future development"
+    filename=$1
+    arguments=${@:2}
+    echo "---------------------------"
+    echo "Running Python filename...."
+    echo "---------------------------"
+    echo
+    python $1 ${@:2}
+    echo
+    echo "---------------------------"
+    echo "Finished !!!"
+    echo "---------------------------"
 }
 
 run_ccpp(){
-    echo "future development"
+
+    filename=$(basename -- "$1")
+    extension="${filename##*.}"
+    name="${filename%.*}"
+
+    echo "Compiling $filename"
+    echo "........."
+    g++ "$1" -o $name
+
+    echo "Running $filename"
+    echo "........."
+    echo
+    ./$name "${@:2}"
+    echo
+    echo "Finished !!!"
 }
 
 # Check if the extension is .sh or .py or .c/.cpp
@@ -36,10 +65,10 @@ if [[ $1 == *.sh ]]; then
     run_shell "$@"
 elif [[ $1 == *.py ]]; then
     run_py "$@"
-elif [[ $1 == *(.c|.cpp) ]]; then
+elif [[ $1 == *.c ]]; then
+    run_ccpp "$@"
+elif [[ $1 == *.cpp ]]; then
     run_ccpp "$@"
 else
     echo "file extension is invalid"
 fi
-
-
