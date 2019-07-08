caf8b6cf6a53786e77b791107e9284b92222339e
Brian Truong <phuongtruong.h@gmail.com>
2019-07-07 19:57:36 -0700

    Update run java, color, and steps

--- script_sh/run_shell.sh
+++ script_sh/run_shell.sh
@@ -7,68 +7,184 @@
 # 1.0       May 26      Run bash script with permission execution
 # 2.0       May 28      Run C/C++ filename
 #                       Run Python filename
-#
+# 3.0       Jun 17      Run Java filename
+# 3.1       Jun 25      Notify testcase failed due to segmentation fault
 #
 # Plans:
-#       1. Run bash script
-#       2. Run Python
-#       3. Run C/C++
+#       1. Create makefile for C/C++ when compiling multi-files and link them -> done
+#       2. Run Python from Jupyter Notebook -> Done
+#       3. Run C/C++ -> done
 #################################################################
+
+# Define color for text
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+filename=$(basename -- "$1")
+name="${filename%.*}"
+extension=$([[ "$filename" = *.* ]] && echo ".${filename##*.}" || echo '')
+logfile="$name".log
 
 # Check if shell script is executable and run it
 run_shell(){
-    if [ -x $1 ]; then
-        ./$1 "${@:2}"
+    if [ -x $filename ]; then
+        echo "--- RUNNING ---"
+        echo
+        ./$filename "${@:2}"
     else
-        chmod +x $1
-        ./$1 "${@:2}"
+        echo "--- SETTING UP PERMISSION ---"
+        chmod +x $filename
+        echo "--- RUNNING ---"
+        ./$filename "${@:2}"
     fi
 
     echo
-    echo "Finished !!!"
+    echo -e "--- ${GREEN}FINISHED !!!${NC} ---"
+    echo
 }
 
 run_py(){
-    filename=$1
-    arguments=${@:2}
-    echo "---------------------------"
-    echo "Running Python filename...."
-    echo "---------------------------"
-    echo
-    python $1 ${@:2}
-    echo
-    echo "---------------------------"
-    echo "Finished !!!"
-    echo "---------------------------"
+    # Python extension
+    if [[ $extension == '.py' ]]; then
+
+        echo "--- Running $filename ---"
+        echo
+        python $filename ${@:2}
+        echo
+        echo -e "--- ${GREEN}Finished !!! ---"
+    # Python Jupyter Notebook
+    elif [[ $extension == '.ipynb' ]]; then
+        echo '''
+        This file is Python Notebook from Jupyter
+        Some components need to be install below
+        > pip install nbconvert --user
+        > pip install jupyter --user
+        '''
+        # Convert .ipynb to .py
+        echo "--- START CONVERTINT TO $name.py ---"
+        jupyter nbconvert --to python $filename
+        echo -e "--- ${GREEN}CONVERTING DONE${NC} ---"
+
+        # Run .ipynb
+        ipython3 "$name.py"
+        echo
+        echo -e "--- ${GREEN}FINISHED !!!${NC} ---"
+
+    fi
 }
 
 run_ccpp(){
+    # check if object file is exist.
+    if [[ -f "output" ]]; then
+        echo "Deleting existing object file ....."
+        echo
+        rm -rf ./output
+    fi
 
-    filename=$(basename -- "$1")
-    extension="${filename##*.}"
-    name="${filename%.*}"
+    # check if previous logfile is exist
+    if [[ -f "$logfile" ]]; then
+        echo "Deleting existing log file $logfile ....."
+        echo
+        rm -rf ./$logfile
+    fi
 
-    echo "Compiling $filename"
-    echo "........."
-    g++ "$1" -o $name
+    # Divide it to run all / run alone and clean
+    echo "--- COMPILING ---"
+    if [[ $extension == '.c' ]]; then
+        g++ *.c -o output 2> $logfile
+    elif [[ $extension == '.cpp' ]]; then
+        g++ *.cpp -o output 2> $logfile
+    fi
 
-    echo "Running $filename"
-    echo "........."
-    echo
-    ./$name "${@:2}"
-    echo
-    echo "Finished !!!"
+    # Check if there is Error in compilation log
+    if egrep -qE "(error|undefined)" $logfile ; then
+        echo
+        echo -e "*** ${RED}COMPILED ERROR !!!${NC} ***"
+        cat $logfile | egrep "(error|undefined)"
+        exit 1
+    else
+        if grep -q "warning" $logfile ; then
+            echo
+            echo -e "*** ${YELLOW}WARNING !!!${NC} ***: Have a look !!!!"
+            cat $logfile | grep "warning"
+        fi
+
+        echo "--- RUNNING ---"
+        echo
+        ./output |& tee -a $logfile
+        echo
+        if grep -q "fault" $logfile ; then
+            echo
+            echo -e "--- ${RED}FAILED !!!${NC} ---"
+            echo
+        else
+            echo
+            echo -e "--- ${GREEN}PASSED !!!${NC} ---"
+            echo
+        fi
+    fi
 }
 
-# Check if the extension is .sh or .py or .c/.cpp
-if [[ $1 == *.sh ]]; then
+run_java(){
+    # Check and remove previous .class file
+
+     if [[ -f "$name.class" ]]; then
+         echo "Deleting existing $name.class ....."
+         echo
+         rm -rf ./$name.class
+     fi
+
+     # check if previous logfile is exist
+     if [[ -f "$logfile" ]]; then
+         echo "Deleting existing $logfile ....."
+         echo
+         rm -rf ./$logfile
+     fi
+
+    # Compile java file
+    echo "--- COMPILING ---"
+    javac $filename 2> $logfile
+
+    # Checking error in the logfile
+    if egrep -qE "error|undefined" $logfile ; then
+         echo
+         echo -e "*** ${RED}COMPILED ERROR !!!${NC} ***"
+         cat $logfile | egrep "error|undefined"
+         exit 1
+    else
+         if grep -q -i "warning" $logfile ; then
+             echo
+             echo -e "*** ${YELLOW}WARNING !!!${NC} ***: Have a look !!!!"
+             cat $logfile | grep "warning"
+         fi
+
+         echo "--- RUNNING ---"
+         echo
+         java $name
+         echo
+         echo -e "--- ${GREEN}FINISHED !!!${NC} ---"
+         echo
+    fi
+}
+
+#######################################################
+#                    MAIN HERE
+#######################################################
+
+if [[ $filename == *.sh ]]; then       # Check if target file is a shell script
     run_shell "$@"
-elif [[ $1 == *.py ]]; then
+elif [[ $filename == *.py ]]; then     # Check if target file is python extension
     run_py "$@"
-elif [[ $1 == *.c ]]; then
+elif [[ $filename == *.ipynb ]]; then  # Check if target file is from Python Jupyter Notebook
+    run_py "$@"
+elif [[ $filename == *.c ]]; then      # Check if target file is c extension
     run_ccpp "$@"
-elif [[ $1 == *.cpp ]]; then
+elif [[ $filename == *.cpp ]]; then    # Check if target file is cpp extension
     run_ccpp "$@"
+elif [[ $filename == *.java ]]; then   # Check if target file is java extension
+    run_java "$@"
 else
     echo "file extension is invalid"
 fi
